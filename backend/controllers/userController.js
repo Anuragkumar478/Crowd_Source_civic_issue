295e36d478add0117901fe66b1ccb2950c741f0e
Anurag kumar <kumaranurag9795@gmail.com>
2025-11-07 09:24:01 +0530

    after adding the true

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,7 +44,7 @@ export const loginUser = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: false, // set true if using https
+        secure: true, // set true if using https
         sameSite: "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
